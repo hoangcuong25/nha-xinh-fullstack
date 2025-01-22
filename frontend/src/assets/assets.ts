71c0ebcb53fbe452aaf372669275798463cf55c2
hoang cuong <vancuong25112003@gmail.com>
2025-01-22 07:21:21 +0700

    update navbar

---
+++ frontend/src/assets/assets.ts
@@ -0,0 +1,165 @@
+export const phong_khach: { title: string; href: string; }[] = [
+    {
+        title: "Sofa",
+        href: "/danh-muc/phong-khach/sofa",
+    },
+    {
+        title: "Sofa góc",
+        href: "/danh-muc/phong-khach/sofa-goc",
+    },
+    {
+        title: "Ghế thư giãn",
+        href: "/danh-muc/phong-khach/ghe-thu-gian",
+    },
+    {
+        title: "Armchair",
+        href: "/danh-muc/phong-khach/armchair",
+    },
+    {
+        title: "Ghế dài & đôn",
+        href: "/danh-muc/phong-khach/ghe-dai-&-don",
+    },
+    {
+        title: "Bàn bên",
+        href: "/danh-muc/phong-khach/ban-ben",
+    },
+    {
+        title: "Bàn nước",
+        href: "/danh-muc/phong-khach/ban-nuoc",
+    },
+]
+
+export const phong_an: { title: string; href: string; }[] = [
+    {
+        title: "Bàn ăn",
+        href: "/danh-muc/phong-an/ban-an",
+    },
+    {
+        title: "Ghế ăn",
+        href: "/danh-muc/phong-an/ghe-an",
+    },
+    {
+        title: "Ghế bar",
+        href: "/danh-muc/phong-an/ghe-bar",
+    },
+    {
+        title: "Tủ ly",
+        href: "/danh-muc/phong-an/tu-ly",
+    },
+    {
+        title: "Tủ bếp",
+        href: "/danh-muc/phong-an/tu-bep",
+    },
+    {
+        title: "Thiết bị bếp",
+        href: "/danh-muc/phong-an/thiet-bi-bep",
+    },
+]
+
+export const phong_ngu: { title: string; href: string; }[] = [
+    {
+        title: "Giường ngủ",
+        href: "/danh-muc/phong-ngu/giuong-ngu",
+    },
+    {
+        title: "Bàn đầu giường",
+        href: "/danh-muc/phong-ngu/ban-dau-giuong",
+    },
+    {
+        title: "Bàn trang điểm",
+        href: "/danh-muc/phong-ngu/ban-trang-diem",
+    },
+    {
+        title: "Tủ quần áo",
+        href: "/danh-muc/phong-ngu/tu-quan-ao",
+    },
+    {
+        title: "Tủ âm tường",
+        href: "/danh-muc/phong-ngu/tu-am-tuong",
+    },
+    {
+        title: "Tủ hộc kéo",
+        href: "/danh-muc/phong-ngu/tu-hoc-keo",
+    },
+    {
+        title: "Kệ",
+        href: "/danh-muc/phong-ngu/ke",
+    },
+]
+
+export const phong_lam_viec: { title: string; href: string; }[] = [
+    {
+        title: "Bàn làm việc",
+        href: "/danh-muc/phong-lam-viec/ban-lam-viec",
+    },
+    {
+        title: "Ghế làm việc",
+        href: "/danh-muc/phong-lam-viec/ghe-lam-viec",
+    },
+    {
+        title: "Kệ sách",
+        href: "/danh-muc/phong-lam-viec/ke-sach",
+    },
+    {
+        title: "Bàn ngoài trời",
+        href: "/danh-muc/phong-lam-viec/ban-ngoai-troi",
+    },
+    {
+        title: "Ghế ngoài trời",
+        href: "/danh-muc/phong-lam-viec/ghe-ngoai-troi",
+    },
+]
+
+export const hang_trang_tri: { title: string; href: string; }[] = [
+    {
+        title: "Đèn",
+        href: "/danh-muc/hang-trang-tri/den",
+    },
+    {
+        title: "Thảm trang trí",
+        href: "/danh-muc/hang-trang-tri/tham-trang-tri",
+    },
+    {
+        title: "Bình trang trí",
+        href: "/danh-muc/hang-trang-tri/binh-trang-tri",
+    },
+    {
+        title: "Tranh",
+        href: "/danh-muc/hang-trang-tri/tranh",
+    },
+    {
+        title: "Gương",
+        href: "/danh-muc/hang-trang-tri/guong",
+    },
+    {
+        title: "Hoa và cây",
+        href: "/danh-muc/hang-trang-tri/hoa-va-cay",
+    },
+    {
+        title: "Đồng hồ",
+        href: "/danh-muc/hang-trang-tri/dong-ho",
+    },
+]
+
+export const danh_muc: { title: string; href: string; }[] = [
+    {
+        title: "Phòng khách",
+        href: "/phong-khach",
+    },
+    {
+        title: "Phòng ăn",
+        href: "/phong-an",
+    },
+    {
+        title: "Phòng ngủ",
+        href: "/phong-ngu",
+    },
+    {
+        title: "Phòng làm việc",
+        href: "/phong-khach",
+    },
+    {
+        title: "Hàng trang trí",
+        href: "/hang-trang-tri",
+    },
+]
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,107 +2,147 @@
 
 import Image from "next/image";
 import { IoMenu } from "react-icons/io5";
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
 import Link from "next/link"
 import { CiSearch } from "react-icons/ci";
-
-const col1: { title: string; href: string; }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-    },
-]
+import { phong_khach, phong_an, phong_ngu, phong_lam_viec, hang_trang_tri, danh_muc } from "@/assets/assets";
+import { IoIosArrowUp } from "react-icons/io";
 
 const Navbar = () => {
     return (
-        <div className='mx-3.5 sm:mx-7 mt-5 flex justify-between items-center border-b border-gray-200 pb-3.5'>
-            <div className='flex gap-5'>
+        <div className='flex justify-between items-center mx-3.5 sm:mx-7 mt-5  border-b border-gray-200 pb-3.5'>
+            <div className='flex gap-7'>
                 <div className='flex items-center gap-3.5'>
                     <IoMenu className='text-3xl' />
                     <Image src='/logo-nhaxinh-25nam-final.png' width='150' height='100' alt="logo" />
                 </div>
 
-                <NavigationMenu className='text-sm'>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className='hover:text-yellow-400'>SẢN PHẨM</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                                    <li className="row-span-3">
-                                        <NavigationMenuLink asChild>
-                                            <Link
-                                                className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                                href="/"
-                                            >
-                                                <div className="mb-2 mt-4 text-lg font-medium">
-                                                    shadcn/ui
-                                                </div>
-                                            </Link>
-                                        </NavigationMenuLink>
-                                    </li>
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className='hover:text-yellow-400'>PHÒNG</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                                    {col1.map((col1) => (
-                                        <NavigationMenuLink className='flex gap-2 items-center'
-                                            key={col1.title}
-                                            title={col1.title}
-                                            href={col1.href}
-                                        >
-                                        </NavigationMenuLink>
-                                    ))}
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/docs" legacyBehavior passHref>
-                                <NavigationMenuLink className='hover:text-yellow-400 mx-2'>
-                                    BỘ SƯU TẬP
-                                </NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/docs" legacyBehavior passHref>
-                                <NavigationMenuLink className='hover:text-yellow-400 mx-2'>
-                                    THIẾT KẾ NỘI THẤT
-                                </NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/docs" legacyBehavior passHref>
-                                <NavigationMenuLink className='hover:text-yellow-400 mx-2'>
-                                    CỬA HÀNG 360 ĐỘ
-                                </NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Link href="/docs" legacyBehavior passHref>
-                                <NavigationMenuLink className='hover:text-yellow-400 mx-2'>
-                                    GÓC CẢM HỨNG
-                                </NavigationMenuLink>
-                            </Link>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
+                <div className='flex items-center text-sm  relative'>
+                    <div className='group'>
+                        <div className='flex items-center gap-2 p-3 group-hover:text-yellow-400 group-hover:bg-gray-50 rounded-md'>
+                            <p className=''>SẢN PHẨM</p>
+                            <IoIosArrowUp className='group-hover:rotate-180 transition-all duration-500' />
+                        </div>
 
+                        <div className='absolute z-50 top-10 text-[16px] hidden group-hover:flex bg-white border border-gray-200 w-[1000px]'>
+                            <div className='flex flex-col gap-5 p-7 text-nowrap '>
+                                {phong_an.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            <div className='flex flex-col gap-5 p-7 text-nowrap'>
+                                {phong_khach.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            <div className='flex flex-col gap-5 p-7 text-nowrap'>
+                                {phong_ngu.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            <div className='flex flex-col gap-5 p-7 text-nowrap'>
+                                {phong_lam_viec.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+
+                            <div className='flex flex-col gap-5 p-7 text-nowrap'>
+                                {hang_trang_tri.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+
+
+                            <div className='p-7'>
+                                <Image src='/nha-xinh-phong-khach-hien-dai-poppy-1-1200x800.jpg' width='300' height='300' alt="banner" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='group relative'>
+                        <div className='flex items-center gap-2 p-3 group-hover:text-yellow-400 group-ghover:bg-gray-50 rounded-md'>
+                            <p>PHÒNG</p>
+                            <IoIosArrowUp className='group-hover:rotate-180 transition-all duration-500' />
+                        </div>
+
+                        <div className='absolute z-50 top-10 left-0 text-[16px] hidden group-hover:flex bg-white border border-gray-200 w-fit'>
+                            <div className='flex flex-col gap-5 p-7 text-nowrap'>
+                                {danh_muc.map((i) => (
+                                    <Link className='flex gap-2 items-center'
+                                        key={i.title}
+                                        title={i.title}
+                                        href={i.href}
+                                    >
+                                        <p className='text-gray-500 hover:text-gray-900'>{i.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <Link
+                        href={'/bo-suu-tap'}
+                        className='text-sm p-3 hover:text-yellow-400 hover:bg-gray-50 rounded-md'
+                    >
+                        BỘ SƯU TẬP
+                    </Link>
+                    <Link
+                        href={'/bo-suu-tap'}
+                        className='text-sm p-3 hover:text-yellow-400 hover:bg-gray-50 rounded-md'
+                    >
+                        THIẾT KẾ NỘI THẤT
+                    </Link>
+                    <Link
+                        href={'/bo-suu-tap'}
+                        className='text-sm p-3 hover:text-yellow-400 hover:bg-gray-50 rounded-md'
+                    >
+                        CỬA HÀNG 360 ĐỘ
+                    </Link>
+                    <Link
+                        href={'/bo-suu-tap'}
+                        className='text-sm p-3 hover:text-yellow-400 hover:bg-gray-50 rounded-md'
+                    >
+                        GÓC CẢM HỨNG
+                    </Link>
+                </div>
+            </div >
             <div className='relative flex items-center '>
                 <input type="text" placeholder="Tìm sản phẩm" className='w-60 h-11 pl-3 pr-9 border border-gray-300 bg-gray-100 rounded-3xl focus:outline-none' />
                 <CiSearch className='text-2xl absolute top-2.5 right-2' />
             </div>
-        </div>
+        </div >
     )
 }
 
