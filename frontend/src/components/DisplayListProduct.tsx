3037b94d7575b5157c7d8967cfa36221fa56c932
hoang cuong <vancuong25112003@gmail.com>
2025-01-25 06:24:25 +0700

    update

--- frontend/src/components/DisplayListProduct.tsx
+++ frontend/src/components/DisplayListProduct.tsx
@@ -58,28 +58,28 @@ const DisplayListProduct: React.FC<IProps> = ({ tittle, products }) => {
 
             <Slider {...setting}>
                 {products.slice(0, 5).map((product: any, index) => (
-                    <div className='group relative mt-5 h-[460px]' key={index}>
-                        <div className='flex flex-col items-center justify-start mt-5 mx-3.5 lg:mx-7 rounded-md cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
+                    <div className='group relative mt-5' key={index}>
+                        <div className='flex flex-col items-center justify-between h-[460px] mt-5 mx-3.5 lg:mx-7 rounded-md cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
                             <Image src={product.image} alt={product.name} className='size-56 object-cover' />
                             <p className='w-full text-start font-semibold mt-2 text-[13px] sm:text-base text-gray-800'>{product.name}</p>
                             <div className='flex flex-col sm:flex-row sm:gap-3 text-start w-full text-[13px] sm:text-base'>
                                 <p className='mt-2 font-thin text-gray-500 line-through'>{product.oldPrice} vnđ</p>
                                 <p className='mt-2 font-semibold text-yellow-500'>{product.newPrice} vnđ</p>
                             </div>
-                        </div>
 
-                        <div className='flex flex-col sm:flex-row justify-end sm:justify-between px-1 lg:px-3.5 mx-3 mt-2 transition-all duration-300 group-hover:-translate-y-5'>
-                            <Button
-                                className='mt-3 bg-yellow-500 rounded-[8px] text-[13px] lg:text-base '
-                            >
-                                Thêm vào giỏ
-                            </Button>
+                            <div className='flex flex-col sm:flex-row justify-end sm:justify-between w-full mt-2 transition-all duration-300 group-hover:-translate-y-5'>
+                                <Button
+                                    className='mt-3 bg-yellow-500 rounded-[8px] text-[13px] lg:text-base '
+                                >
+                                    Thêm vào giỏ
+                                </Button>
 
-                            <Button
-                                className='mt-3 bg-white hover:text-white text-black border border-black rounded-[8px] text-[13px] lg:text-base'
-                            >
-                                Xem nhanh
-                            </Button>
+                                <Button
+                                    className='mt-3 bg-white hover:text-white text-black border border-black rounded-[8px] text-[13px] lg:text-base'
+                                >
+                                    Xem nhanh
+                                </Button>
+                            </div>
                         </div>
 
                         <div className='w-full flex justify-between px-3.5 lg:px-7 absolute top-7 right-0 cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
