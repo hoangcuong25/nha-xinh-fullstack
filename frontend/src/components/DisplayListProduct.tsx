5d4a7b6d4bdcd35bafc3fadfc0985bc503e95747
hoang cuong <vancuong25112003@gmail.com>
2025-01-24 09:29:28 +0700

    responsive hero

--- frontend/src/components/DisplayListProduct.tsx
+++ frontend/src/components/DisplayListProduct.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { CiHeart, CiSearch } from "react-icons/ci";
@@ -7,6 +9,9 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface IProps {
     tittle: string,
@@ -14,43 +19,71 @@ interface IProps {
 }
 
 const DisplayListProduct: React.FC<IProps> = ({ tittle, products }) => {
+
+    const setting = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3,
+                    speed: 1000,
+                    infinite: true,
+                    autoplay: true,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    speed: 1000,
+                    infinite: true,
+                    autoplay: true,
+                }
+            }
+        ]
+    }
+
+
     return (
         <div>
-            <p className='text-2xl font-medium text-center underline underline-offset-8 mt-16 text-gray-700'>{tittle}</p>
+            <p className='text-xl sm:text-2xl font-medium text-center underline underline-offset-8 mt-16 text-gray-700'>{tittle}</p>
 
-            <div className='flex flex-wrap justify-between mt-5'>
-                {products.map((product: any, index) => (
-                    <div className='group relative' key={index}>
-                        <div className='flex flex-col items-center justify-start mt-5 rounded-md cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
+            <Slider {...setting}>
+                {products.slice(0, 5).map((product: any, index) => (
+                    <div className='group relative mt-5' key={index}>
+                        <div className='flex flex-col items-center justify-start mt-5 mx-3.5 lg:mx-7 rounded-md cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
                             <Image src={product.image} alt={product.name} className='size-56 object-cover' />
-                            <p className='w-full text-start font-semibold mt-2'>{product.name}</p>
-                            <div className='flex gap-3 text-start w-full'>
+                            <p className='w-full text-start font-semibold mt-2 text-[13px] sm:text-base'>{product.name}</p>
+                            <div className='flex flex-col sm:flex-row sm:gap-3 text-start w-full text-[13px] sm:text-base'>
                                 <p className='mt-2 font-thin text-gray-500 line-through'>{product.oldPrice} vnđ</p>
                                 <p className='mt-2 font-semibold text-yellow-500'>{product.newPrice} vnđ</p>
                             </div>
                         </div>
 
-                        <div className='flex items-center justify-between transition-all duration-300 group-hover:-translate-y-5'>
+                        <div className='flex flex-col sm:flex-row items-center justify-between px-1 lg:px-3.5 mx-3 mt-2 transition-all duration-300 group-hover:-translate-y-5'>
                             <Button
-                                className='mt-3 bg-yellow-500 rounded-[8px] '
+                                className='mt-3 bg-yellow-500 rounded-[8px] text-[13px] lg:text-base '
                             >
                                 Thêm vào giỏ
                             </Button>
 
-                            <TooltipProvider>
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <CiSearch className='text-xl' />
-                                    </TooltipTrigger>
-                                    <TooltipContent className='px-3.5 py-2 rounded-[7px]'>
-                                        <p>Xem nhanh</p>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </TooltipProvider>
+                            <Button
+                                className='mt-3 bg-white hover:text-white text-black border border-black rounded-[8px] text-[13px] lg:text-base'
+                            >
+                                Xem nhanh
+                            </Button>
                         </div>
 
-                        <div className='w-full flex justify-between px-2 absolute top-7 right-0 cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
-                            <div className='px-2.5 py-1.5 bg-yellow-400 rounded-[12px] text-white text-xs'>
+                        <div className='w-full flex justify-between px-3.5 lg:px-7 absolute top-7 right-0 cursor-pointer transition-all duration-300 group-hover:-translate-y-5'>
+                            <div className='px-3 py-1.5 bg-yellow-400 rounded-[12px] text-white text-xs'>
                                 -10%
                             </div>
 
@@ -67,8 +100,8 @@ const DisplayListProduct: React.FC<IProps> = ({ tittle, products }) => {
                         </div>
                     </div>
                 ))}
-            </div>
-        </div>
+            </Slider>
+        </div >
     )
 }
 
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -27,83 +27,65 @@ const Hero = () => {
             <DisplayListProduct tittle='NỘI THẤT PHÒNG ĂN' products={products} />
             <DisplayListProduct tittle='PHỤ KIỆN TRANG TRÍ' products={products} />
 
-            <p className='text-2xl font-medium text-center underline underline-offset-8 mt-16 mb-8 text-gray-700'>BỘ SƯU TẬP</p>
+            <p className='text-xl sm:text-2xl font-medium text-center underline underline-offset-8 mt-16 mb-8 text-gray-700'>BỘ SƯU TẬP</p>
 
-            <div className='flex flex-wrap justify-between gap-10'>
-                <Image src={collect_1} alt='collection' className='size-[420px] rounded-[10px] object-cover' />
-                <Image src={collect_2} alt='collection' className='size-[420px] rounded-[10px] object-cover' />
+            <div className='flex flex-wrap justify-center gap-10'>
+                <Image src={collect_1} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px]  rounded-[10px] object-cover' />
+                <Image src={collect_2} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px] rounded-[10px] object-cover' />
                 <div className='relative'>
-                    <Image src={collect_5} alt='collection' className='size-[420px] rounded-[10px] opacity-80 object-cover' />
-                    <div className='absolute top-10 left-3.5 text-gray-900'>
-                        <p className='font-semibold text-3xl'>Thiết kế</p>
-                        <p className='my-2.5 text-stone-600 text-lg font-medium'>Sang trọng và giản dị </p>
-                        <p>Nơi phong cách tối giản gặp gỡ sự tinh tế hiện đại,
+                    <Image src={collect_5} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px] rounded-[10px] opacity-80 object-cover' />
+                    <div className='absolute top-2 lg:top-10 left-1.5 lg:left-3.5 text-xs lg:text-base text-gray-900'>
+                        <p className='font-semibold text-base lg:text-3xl'>Thiết kế</p>
+                        <p className='my-1 lg:my-2.5 text-stone-600 text-sm lg:text-lg font-medium'>Sang trọng và giản dị </p>
+                        <p className='sm:block hidden'>Nơi phong cách tối giản gặp gỡ sự tinh tế hiện đại,
                             mỗi sản phẩm đều được chọn lọc kỹ lưỡng với mục tiêu tối ưu hóa không gian sống,
                             đồng thời phản ánh một phong cách sống đẳng cấp.
                         </p>
                     </div>
                 </div>
                 <div className='relative'>
-                    <Image src={collect_6} alt='collection' className='size-[420px] rounded-[10px] opacity-80 object-cover' />
-                    <div className='absolute top-10 left-3.5 text-gray-900 '>
-                        <p className='font-semibold text-3xl'>Chất lượng</p>
-                        <p className='my-2.5 text-stone-600 text-lg font-medium'>Được chọn lọc kỹ lưỡng</p>
-                        <p>Không chỉ mang đến vẻ đẹp thẩm mỹ mà còn đáp ứng các tiêu chuẩn về sự thoải mái và tiện nghi.
+                    <Image src={collect_6} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px] rounded-[10px] opacity-80 object-cover' />
+                    <div className='absolute top-2 lg:top-10 left-1.5 lg:left-3.5 text-xs lg:text-base text-gray-900 '>
+                        <p className='font-semibold text-base lg:text-3xl'>Chất lượng</p>
+                        <p className='my-1 lg:my-2.5 text-stone-600 text-sm lg:text-lg font-medium'>Được chọn lọc kỹ lưỡng</p>
+                        <p className='sm:block hidden'>Không chỉ mang đến vẻ đẹp thẩm mỹ mà còn đáp ứng các tiêu chuẩn về sự thoải mái và tiện nghi.
                         </p>
                     </div>
                 </div>
-                <Image src={collect_3} alt='collection' className='size-[420px] rounded-[10px] object-cover' />
-                <Image src={collect_4} alt='collection' className='size-[420px] rounded-[10px] object-cover' />
+                <Image src={collect_3} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px] rounded-[10px] object-cover' />
+                <Image src={collect_4} alt='collection' className='lg:size-[420px] sm:size-[200px] size-[120px] rounded-[10px] object-cover' />
             </div>
 
-            <p className='text-2xl font-medium text-center underline underline-offset-8 mt-16 mb-8 text-gray-700'>TRẢI NGHIỆM KHÁCH HÀNG</p>
+            <p className='text-xl sm:text-2xl font-medium text-center underline underline-offset-8 mt-16 mb-8 text-gray-700'>GÓC CẢM HỨNG</p>
 
-            <div className='flex gap-5 h-[268px]'>
-                <div className='w-72 h-full rounded-[12px] bg-gray-100 shadow-md p-7'>
-                    <p className='text-5xl text-gray-700 font-semibold mb-3.5'>+ 50</p>
-                    <p className='mb-10'>sản phẩm</p>
-                    <p className='text-5xl text-gray-700 font-semibold mb-3.5'>+200</p>
-                    <p>khách hàng tin dùng</p>
-                </div>
-                <div className='flex justify-between items-center gap-16 w-full h-full rounded-[12px] bg-gray-100 shadow-md p-7'>
-                    <div className='flex flex-col items-center gap-5'>
-                        <CiShoppingCart className='text-gray-700 text-8xl p-3.5 bg-white rounded-full' />
-                        <p className='text-gray-700'>Lựa chọn sản phẩm</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-5'>
-                        <SlEarphones className='text-gray-700 text-8xl p-3.5 bg-white rounded-full' />
-                        <p className='text-gray-700 '>Nhận tư vấn bởi đội ngũ chuyên nghiệp</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-5'>
-                        <CiCreditCard1 className='text-gray-700 text-8xl p-3.5 bg-white rounded-full' />
-                        <p className='text-gray-700'>Thanh toán nhanh chóng, với chương trình hỗ trợ trả góp</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-5'>
-                        <TbTruckDelivery className='text-gray-700 text-8xl p-3.5 bg-white rounded-full' />
-                        <p className='text-gray-700'>Vận chuyển và lắp ráp miễn phí tại gia</p>
-                    </div>
-                </div>
-            </div>
+            <div className='flex flex-col md:flex-row items-center md:items-start justify-between gap-10 md:gap-3'>
+                <div className='flex flex-col items-center gap-5 w-[200px] lg:w-[300px] xl:w-[400px]'>
+                    <Image src={goc_cam_hung_1} alt='collection' className='goc-cam-hung' />
+                    <p className='lg:text-lg text-base font-semibold text-center w-[200px] lg:w-[300px] xl:w-[400px] md:px-3.5 px-0'>Ghế sofa cho không gian nhỏ</p>
+                    <p className='md:px-3.5 px-0'>
+                        Ghế sofa là thực sự cần thiết cho không gian phòng khách ...
 
-            <p className='text-2xl font-medium text-center underline underline-offset-8 mt-16 mb-8 text-gray-700'>GÓC CẢM HỨNG</p>
-
-            <div className='flex justify-between'>
-                <div className='flex flex-col gap-5'>
-                    <Image src={goc_cam_hung_1} alt='collection' className='h-[300px] w-[400px] rounded-[10px] object-cover' />
-                    <p className='text-lg font-semibold text-center w-[400px]'>Ghế sofa cho không gian nhỏ</p>
-                    <p>Ghế sofa là thực sự cần thiết cho không gian phòng khách ...</p>
+                    </p>
                 </div>
 
-                <div className='flex flex-col gap-5'>
-                    <Image src={goc_cam_hung_2} alt='collection' className='h-[300px] w-[400px] rounded-[10px] object-cover' />
-                    <p className='text-lg font-semibold text-center w-[400px]'>Nội thất của căn hộ mang nét đẹp nghệ thuật hài hòa giữa truyền thống và hiện đại</p>
-                    <p>Với nội thất được thiết kế chỉnh chu, căn hộ thuộc sở hữu ...</p>
+                <div className='flex flex-col items-center gap-5 w-[200px] lg:w-[300px] xl:w-[400px]'>
+                    <Image src={goc_cam_hung_2} alt='collection' className='goc-cam-hung' />
+                    <p className='lg:text-lg text-base font-semibold text-center w-[200px] lg:w-[300px] xl:w-[400px] md:px-3.5 px-0'>
+                        Nội thất của căn hộ mang nét đẹp nghệ thuật hài hòa giữa truyền thống và hiện đại
+                    </p>
+                    <p className='md:px-3.5 px-0'>
+                        Với nội thất được thiết kế chỉnh chu, căn hộ thuộc sở hữu ...
+                    </p>
                 </div>
 
-                <div className='flex flex-col gap-5'>
-                    <Image src={goc_cam_hung_3} alt='collection' className='h-[300px] w-[400px] rounded-[10px] object-cover' />
-                    <p className='text-lg font-semibold text-center w-[400px]'>Khám Phá Không Gian Nhà Xinh Thảo Điền</p>
-                    <p>hà Xinh Thảo Điền mang đến một không gian sống hiện đại ...</p>
+                <div className='flex flex-col items-center gap-5 w-[200px] lg:w-[300px] xl:w-[400px]'>
+                    <Image src={goc_cam_hung_3} alt='collection' className='goc-cam-hung' />
+                    <p className='lg:text-lg text-base font-semibold text-center w-[200px] lg:w-[300px] xl:w-[400px] md:px-3.5 px-0'>
+                        Khám Phá Không Gian Nhà Xinh Thảo Điền
+                    </p>
+                    <p className='md:px-3.5 px-0'>
+                        Nhà Xinh Thảo Điền mang đến một không gian sống hiện đại ...
+                    </p>
                 </div>
             </div>
         </div>
